1dce167146fb851a870de9ca0b4a60acf0fd3a4e
Matthew Dunavant <madunavant@gmail.com>
2019-03-13 09:11:01 -0400

    Models made for Sql tables with properties corresponding to column names

---
+++ WebApplication.Web/Models/Park.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class Park
+    {
+        public string ParkCode { get; set; }
+        public string ParkName { get; set; }
+        public string State { get; set; }
+        public int Acreage { get; set; }
+        public int ElevationInFeet { get; set; }
+        public double MilesOfTrail { get; set; }
+        public int NumberOfCampsites { get; set; }
+        public string Climate { get; set; }
+        public int YearFounded { get; set; }
+        public int AnnualVisitorCount { get; set; }
+        public string InspirationalQuote { get; set; }
+        public string InspirationalQuoteSource { get; set; }
+        public string ParkDescription { get; set; }
+        public int EntryFee { get; set; }
+        public int NumberOfAnimalSpecies { get; set; }
+    }
+}
