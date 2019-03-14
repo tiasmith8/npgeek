048eab2c171c0ed884e39d90fc899c6f1bbf9343
Matthew Dunavant <madunavant@gmail.com>
2019-03-14 14:41:44 -0400

    JavaScript and cookies working

--- WebApplication.Web/wwwroot/js/site.js
+++ WebApplication.Web/wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function checkCelsius() {
+    var allCookies = document.cookie;
+    var cookiearray = allCookies.split(';');
+    var toCelsius;
+
+    for (var i = 0; i < cookiearray.length; i++) {
+        name = cookiearray[i].split('=')[0];
+        value = cookiearray[i].split('=')[1];
+        if (name == "toCelsius") {
+            toCelsius = value;
+        }
+    }
+
+    if (toCelsius == "true") {
+        document.getElementById("tempConvert").checked = true;
+        setTemp();
+    } else {
+        document.getElementById("tempConvert").checked = false;
+    }
+}
+
+function setTemp() {
+    var checkBox = document.getElementById("tempConvert");
+    var i;
+    if (checkBox.checked) {
+        document.cookie = "toCelsius=true; path=/";
+
+        for (i = 0; i < 5; i++) {
+            var text = document.getElementById("high-temp-day-" + i);
+            text.innerHTML = Math.round((text.innerHTML - 32) * 5 / 9);
+
+            var text = document.getElementById("low-temp-day-" + i);
+            text.innerHTML = Math.round((text.innerHTML - 32) * 5 / 9);
+
+            document.getElementById("degree-type-high-day-" + i).innerHTML = " &#8451";
+            document.getElementById("degree-type-low-day-" + i).innerHTML = " &#8451";
+        }
+    } else {
+        document.cookie = "toCelsius=false; path=/";
+
+        for (i = 0; i < 5; i++) {
+            var text = document.getElementById("high-temp-day-" + i);
+            text.innerHTML = Math.round((text.innerHTML * 9 / 5 + 32));
+
+            var text = document.getElementById("low-temp-day-" + i);
+            text.innerHTML = Math.round((text.innerHTML * 9 / 5 + 32));
+
+            document.getElementById("degree-type-high-day-" + i).innerHTML = " &#8457";
+            document.getElementById("degree-type-low-day-" + i).innerHTML = " &#8457";
+        }
+    }
+}
