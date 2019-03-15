5f38c0f0da63bc5693db588d7f9d97747af38827
Tia Smith <tia.smith@gmail.com>
2019-03-15 10:58:34 -0400

    Created test for SubmitSurveyShouldReturnTrue

--- WebApplication.Tests/DAL/SurveySqlDAOTests.cs
+++ WebApplication.Tests/DAL/SurveySqlDAOTests.cs
@@ -20,7 +20,29 @@ namespace WebApplication.Tests.DAL
             IList<SurveyResults> listOfSurveys = dao.GetAllSurveys();
 
             // Assert - should return one survey
-            Assert.AreEqual(listOfSurveys.Count, 1);
+            Assert.AreEqual(listOfSurveys.Count, 1, "Querying for one survey should return one survey");
         }
+
+        [TestMethod]
+        public void SubmitSurveyShouldReturnTrue()
+        {
+            // Arrange
+            SurveySqlDAO dao = new SurveySqlDAO(ConnectionString);
+
+            // Act - create a new survey
+            Survey survey = new Survey();
+            survey.EmailAddress = "test.email@gmail.com";
+            survey.ActivityLevel = "active";
+            survey.ParkCode = "ABC";
+            survey.State = "VA";
+            bool actual = dao.SubmitSurvey(survey);
+
+            // Assert - sending new survey should return true
+            Assert.AreEqual(true, actual, "Creating new survey should return true");
+
+        }
+
+
+
     }
 }
