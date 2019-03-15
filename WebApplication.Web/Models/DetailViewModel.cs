b6587e7e5a904cef357704ccad30321926482f78
Matthew Dunavant <madunavant@gmail.com>
2019-03-15 09:59:44 -0400

    Added comments to Models

--- WebApplication.Web/Models/DetailViewModel.cs
+++ WebApplication.Web/Models/DetailViewModel.cs
@@ -7,7 +7,14 @@ namespace WebApplication.Web.Models
 {
     public class DetailViewModel
     {
+        /// <summary>
+        /// Park with all details to be shown on page
+        /// </summary>
         public Park park { get; set; }
+
+        /// <summary>
+        /// 5-Day forecast for park to be displayed on page
+        /// </summary>
         public IList<Weather> forecast { get; set; }
     }
 }
--- WebApplication.Web/Models/Park.cs
+++ WebApplication.Web/Models/Park.cs
@@ -7,20 +7,79 @@ namespace WebApplication.Web.Models
 {
     public class Park
     {
+        /// <summary>
+        /// Code that uniquely identifies Park
+        /// </summary>
         public string ParkCode { get; set; }
+
+        /// <summary>
+        /// Name of Park
+        /// </summary>
         public string ParkName { get; set; }
+
+        /// <summary>
+        /// State within which the Park is located
+        /// </summary>
         public string State { get; set; }
+
+        /// <summary>
+        /// Total surface area of Park in acres
+        /// </summary>
         public int Acreage { get; set; }
+
+        /// <summary>
+        /// Average elevation above sea level of Park in feet
+        /// </summary>
         public int ElevationInFeet { get; set; }
+
+        /// <summary>
+        /// Total length of hiking trails in miles
+        /// </summary>
         public double MilesOfTrail { get; set; }
+
+        /// <summary>
+        /// Number of campsites within the Park
+        /// </summary>
         public int NumberOfCampsites { get; set; }
+
+        /// <summary>
+        /// Climate of Park
+        /// </summary>
         public string Climate { get; set; }
+
+        /// <summary>
+        /// Year in which the Park was founded
+        /// </summary>
         public int YearFounded { get; set; }
+
+        /// <summary>
+        /// Average annual visitors
+        /// </summary>
         public int AnnualVisitorCount { get; set; }
+
+        /// <summary>
+        /// Quote related to the Park
+        /// </summary>
         public string InspirationalQuote { get; set; }
+
+        /// <summary>
+        /// Source of Quote
+        /// </summary>
         public string InspirationalQuoteSource { get; set; }
+
+        /// <summary>
+        /// Short description of Park
+        /// </summary>
         public string ParkDescription { get; set; }
+
+        /// <summary>
+        /// Fee required for entry to Park
+        /// </summary>
         public int EntryFee { get; set; }
+
+        /// <summary>
+        /// Number of species native to Park
+        /// </summary>
         public int NumberOfAnimalSpecies { get; set; }
     }
 }
