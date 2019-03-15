a3753772c0a0c1d3971ca55f26c45d711936d400
Tia Smith <tia.smith@gmail.com>
2019-03-15 10:51:19 -0400

    Added test GetAllSurveysShouldReturnOneSurvey

---
+++ WebApplication.Tests/DAL/SurveySqlDAOTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Web.DAL;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Tests.DAL
+{
+    [TestClass]
+    public class SurveySqlDAOTests : NPSqlDAOTests
+    {
+        [TestMethod]
+        public void GetAllSurveysShouldReturnOneSurvey()
+        {
+            // Arrange
+            SurveySqlDAO dao = new SurveySqlDAO(ConnectionString);
+
+            // Act - List of surveys should return 1 survey created in test data setup
+            IList<SurveyResults> listOfSurveys = dao.GetAllSurveys();
+
+            // Assert - should return one survey
+            Assert.AreEqual(listOfSurveys.Count, 1);
+        }
+    }
+}
